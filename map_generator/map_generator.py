8ea80793829cb61e1b25e9573d8f0adf8f7a8ac1
Nathan Yang <jumbosizegalaxy@gmail.com>
2025-05-17 00:33:44 -0700

    Tile class

---
+++ map_generator/map_generator.py
@@ -0,0 +1,8 @@
+class Tile:
+    def __init__(self, x, y, z, biome = "plain", terrain = "flat"):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.biome = biome
+        self.terrain = terrain
+
